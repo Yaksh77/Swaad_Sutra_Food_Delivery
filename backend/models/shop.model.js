7c67ea98a96af4475ca68df02250475220850f9b
Yaksh Git <chudasamayaksh77@gmail.com>
2025-09-07 21:20:55 +0530

    feat: Add shop model schema with required fields for name, image, owner, city, state, and address

---
+++ backend/models/shop.model.js
@@ -0,0 +1,33 @@
+import mongoose, { mongo } from "mongoose";
+const shopSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    city: {
+      type: String,
+      require: true,
+    },
+    state: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
