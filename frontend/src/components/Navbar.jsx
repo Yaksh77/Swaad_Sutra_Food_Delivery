b636bac34abe21cb04120aaa7f8aaab6d979ba7c
Yaksh Git <chudasamayaksh77@gmail.com>
2025-09-07 23:32:41 +0530

    feat: Update Navbar and OwnerDashboard components to conditionally render elements based on user role

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,6 +7,8 @@ import { RxCross2 } from "react-icons/rx";
 import { SERVER_API } from "../../api";
 import axios from "axios";
 import { setUserData } from "../redux/user.slice";
+import { CiCirclePlus } from "react-icons/ci";
+import { IoReceiptOutline } from "react-icons/io5";
 
 function Navbar() {
   const { userData, city } = useSelector((state) => state.user);
@@ -26,7 +28,7 @@ function Navbar() {
   };
   return (
     <div className="w-full h-[80px] flex items-center justify-center md:justify-center gap-[30px] px-[20px] fixed top-0 left-0 z-[9999] bg-[#F1F8E9]">
-      {showSearch && (
+      {showSearch && userData.role == "user" && (
         <div className="w-[90%] h-[70px] fixed bg-white shadow-xl rounded-lg items-center gap-[20px] flex top-[80px] left-[5%] md:hidden">
           <div className="flex items-center w-[30%] overflow-hidden gap-[10px] px-[10px] border-r-[2px] border-gray-400">
             <IoLocationOutline size={30} color="#43A047" />
@@ -44,44 +46,77 @@ function Navbar() {
       )}
 
       <h1 className="text-3xl font-bold mb-2 text-[#43A047]">Swaad Sutra</h1>
-      <div className="md:w-[60%] lg:w-[40%] h-[70px] bg-white shadow-xl rounded-lg items-center gap-[20px] hidden md:flex">
-        <div className="flex items-center w-[30%] overflow-hidden gap-[10px] px-[10px] border-r-[2px] border-gray-400">
-          <IoLocationOutline size={30} color="#43A047" />
-          <div className="w-[80%] truncate text-gray-600">{city}</div>
+      {userData.role == "user" && (
+        <div className="md:w-[60%] lg:w-[40%] h-[70px] bg-white shadow-xl rounded-lg items-center gap-[20px] hidden md:flex">
+          <div className="flex items-center w-[30%] overflow-hidden gap-[10px] px-[10px] border-r-[2px] border-gray-400">
+            <IoLocationOutline size={30} color="#43A047" />
+            <div className="w-[80%] truncate text-gray-600">{city}</div>
+          </div>
+          <div className="w-[80%] flex items-center gap-[10px]">
+            <IoSearch size={25} className="text-[#43A047]" />
+            <input
+              type="text"
+              placeholder="Search Delicious Food"
+              className="px-[10px] text-gray-700 outline-0 w-full"
+            />
+          </div>
         </div>
-        <div className="w-[80%] flex items-center gap-[10px]">
-          <IoSearch size={25} className="text-[#43A047]" />
-          <input
-            type="text"
-            placeholder="Search Delicious Food"
-            className="px-[10px] text-gray-700 outline-0 w-full"
-          />
-        </div>
-      </div>
+      )}
+
       <div className="flex items-center gap-4">
-        {showSearch ? (
-          <RxCross2
-            size={25}
-            className="text-[#43A047] md:hidden"
-            onClick={() => setShowSearch(false)}
-          />
+        {userData.role == "user" &&
+          (showSearch ? (
+            <RxCross2
+              size={25}
+              className="text-[#43A047] md:hidden"
+              onClick={() => setShowSearch(false)}
+            />
+          ) : (
+            <IoSearch
+              size={25}
+              className="text-[#43A047] md:hidden"
+              onClick={() => setShowSearch(true)}
+            />
+          ))}
+
+        {userData.role == "owner" ? (
+          <>
+            <button className="hidden md:flex  items-center gap-1 p-2 cursor-pointer rounded-full bg-[#ff4d2d]/10 text-[#43A047]">
+              <CiCirclePlus size={20} />
+              <span>Add Food Item</span>
+            </button>
+            <button className="md:hidden flex  items-center gap-1 p-2 cursor-pointer rounded-full bg-[#ff4d2d]/10 text-[#43A047]">
+              <CiCirclePlus size={20} />
+            </button>
+            <div className="hidden md:flex items-center gap-2 cursor-pointer relative px-3 py-1 rounded-lg bg-[#ff4d2d]/10 text-[#43A047] font-medium">
+              <IoReceiptOutline size={20} />
+              <span>My Orders</span>
+              <span className="absolute -right-2 -top-2 text-xs font-bold text-white bg-[#43A047]/70 rounded-full px-[6px] py-[1px]">
+                0
+              </span>
+            </div>
+            <div className="md:hidden flex items-center gap-2 cursor-pointer relative px-3 py-1 rounded-lg bg-[#ff4d2d]/10 text-[#43A047] font-medium">
+              <IoReceiptOutline size={20} />
+              <span className="absolute -right-2 -top-2 text-xs font-bold text-white bg-[#43A047]/70 rounded-full px-[6px] py-[1px]">
+                0
+              </span>
+            </div>
+          </>
         ) : (
-          <IoSearch
-            size={25}
-            className="text-[#43A047] md:hidden"
-            onClick={() => setShowSearch(true)}
-          />
+          <>
+            <div className="relative cursor-pointer">
+              <FaShoppingCart size={25} className="text-[#43A047]" />
+              <span className="absolute right-[-9px] top-[-12px] text-[#43A047]">
+                0
+              </span>
+            </div>
+
+            <button className="hidden md:block px-3 py-1 rounded-lg  bg-[#ff4d2d]/10 text-[#43A047] text-sm font-medium">
+              My Orders
+            </button>
+          </>
         )}
 
-        <div className="relative cursor-pointer">
-          <FaShoppingCart size={25} className="text-[#43A047]" />
-          <span className="absolute right-[-9px] top-[-12px] text-[#43A047]">
-            0
-          </span>
-        </div>
-        <button className="hidden md:block px-3 py-1 rounded-lg  bg-[#ff4d2d]/10 text-[#43A047] text-sm font-medium">
-          My Orders
-        </button>
         <div
           onClick={() => setShowInfo((prevState) => !prevState)}
           className="w-[40px] h-[40px] rounded-full flex items-center justify-center bg-[#43A047] text-white text-[18px] shadow-xl font-semibold cursor-pointer"
--- frontend/src/components/OwnerDashboard.jsx
+++ frontend/src/components/OwnerDashboard.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 
 function OwnerDashboard() {
-  return <div>OwnerDashboard</div>;
+  return (
+    <div className="w-full min-h-screen bg-[#F1F8E9] flex flex-col items-center">
+      <Navbar />
+    </div>
+  );
 }
 
 export default OwnerDashboard;
