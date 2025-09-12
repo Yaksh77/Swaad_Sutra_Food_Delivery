58b1ac37fc8e3e162a45dc93d12162adb9898248
Yaksh Git <chudasamayaksh77@gmail.com>
2025-09-13 01:05:51 +0530

    feat: Implement EditItem component for editing food items with image upload and loading state

--- frontend/src/pages/EditItem.jsx
+++ frontend/src/pages/EditItem.jsx
@@ -1,20 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FiArrowLeftCircle } from "react-icons/fi";
 import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { FaUtensils } from "react-icons/fa";
 import axios from "axios";
 import { SERVER_API } from "../../api";
 import { setShopData } from "../redux/owner.slice";
 import { ClipLoader } from "react-spinners";
 
-function AddItem() {
+function EditItem() {
   const navigate = useNavigate();
   const { shopData } = useSelector((state) => state.owner);
+  const { itemId } = useParams();
+  const [currentItem, setCurrentItem] = useState(null);
   const [name, setName] = useState("");
   const [price, setPrice] = useState(0);
   const [category, setCategory] = useState("");
-  const [foodType, setFoodType] = useState("veg");
+  const [foodType, setFoodType] = useState("");
+  const [frontendImage, setFrontendImage] = useState(currentItem?.image || "");
+  const [backendImage, setBackendImage] = useState(null);
   const [loading, setLoading] = useState(false);
 
   const categories = [
@@ -34,8 +38,6 @@ function AddItem() {
     "Biryani",
     "Street Food",
   ];
-  const [frontendImage, setFrontendImage] = useState(null);
-  const [backendImage, setBackendImage] = useState(null);
   const dispatch = useDispatch();
 
   const handleImage = (e) => {
@@ -46,7 +48,6 @@ function AddItem() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
-
     try {
       const formData = new FormData();
       formData.append("name", name);
@@ -57,22 +58,47 @@ function AddItem() {
         formData.append("image", backendImage);
       }
 
-      const result = await axios.post(`${SERVER_API}/item/add-item`, formData, {
-        withCredentials: true,
-        headers: { "Content-Type": "multipart/form-data" },
-      });
-
-      setName("");
-      setCategory("");
-      setPrice(0);
-      setFrontendImage(null);
-      setLoading(false);
+      const result = await axios.put(
+        `${SERVER_API}/item/edit-item/${itemId}`,
+        formData,
+        {
+          withCredentials: true,
+        }
+      );
       dispatch(setShopData(result.data));
+      setLoading(false);
+      navigate("/");
     } catch (error) {
       console.log("Error occurred while processing the form data");
       setLoading(false);
     }
   };
+
+  useEffect(() => {
+    const handleGetItemById = async () => {
+      try {
+        const result = await axios.get(
+          `${SERVER_API}/item/get-item/${itemId}`,
+          {
+            withCredentials: true,
+          }
+        );
+        setCurrentItem(result.data);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    handleGetItemById();
+  }, [itemId]);
+
+  useEffect(() => {
+    setName(currentItem?.name || "");
+    setPrice(currentItem?.price || 0);
+    setCategory(currentItem?.category || "");
+    setFoodType(currentItem?.foodType || "");
+    setFrontendImage(currentItem?.image || "");
+  }, [currentItem]);
+
   return (
     <div className="flex justify-center flex-col items-center p-6 bg-gradient-to-br from-green-50 relative to-white min-h-screen">
       <div className="absolute top-[20px] left-[20px] z-[10] mb-[10px]">
@@ -87,7 +113,9 @@ function AddItem() {
           <div className="bg-green-100 p-4 rounded-full mb-4">
             <FaUtensils className="text-[#43A047] w-16 h-16" />
           </div>
-          <div className="text-3xl font-extrabold text-gray-900">Add Food</div>
+          <div className="text-3xl font-extrabold text-gray-900">
+            Edit Food Item
+          </div>
         </div>
         <form className="space-y-5" onSubmit={handleSubmit}>
           <div>
@@ -99,7 +127,7 @@ function AddItem() {
             </label>
             <input
               type="text"
-              placeholder="Enter Food Name"
+              placeholder="Enter Shop Name"
               className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
               value={name}
               onChange={(e) => setName(e.target.value)}
@@ -186,7 +214,10 @@ function AddItem() {
               })}
             </select>
           </div>
-          <button className="w-full text-lg bg-gray-500 text-white px-6 py-2 rounded-lg font-semibold shadow-md hover:bg-green-700 hover-shadow-lg transition-all cursor-pointer">
+          <button
+            disabled={loading}
+            className="w-full text-lg bg-gray-500 text-white px-6 py-2 rounded-lg font-semibold shadow-md hover:bg-green-700 hover-shadow-lg transition-all cursor-pointer"
+          >
             {loading ? <ClipLoader size={20} color="white" /> : "Save"}
           </button>
         </form>
@@ -195,4 +226,4 @@ function AddItem() {
   );
 }
 
-export default AddItem;
+export default EditItem;
